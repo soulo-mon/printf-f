f5b6b019e3edab6e861d8d2a3dd4d96b5a930d34
Solomon <Solomont98@yahoo.com>
2022-10-17 08:39:09 -0700

    Header file updated

--- main.h
+++ main.h
@@ -1,35 +1,46 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
 
 /**
- * struct printer - structure for printing to stdout, given a specifier
- * @specifier: the specifier recognized by this printer (without the %)
- * @run: the function to run when this printer is invoked
+ * struct format - match each conversion specifier for printf
+ * @id: type char pointer of the specifier i.e (l,h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier.
  */
-typedef struct printer
+
+typedef struct format
 {
-	char *specifier;
-	int (*run)(va_list);
-} printer;
+	char *id;
+	int (*f)();
+} convert_match;
 
-printer _get_printer(const char *specifier);
-int _putchar(char c);
 int _printf(const char *format, ...);
-int _print_char(va_list);
-int _print_str(va_list);
-int _print_decimal(va_list);
-int _print_int(va_list);
-int _print_binary(va_list);
-int _print_str_nonprintable(va_list);
-int _print_str_reverse(va_list);
-int _print_rot13(va_list);
-int _print_uint(va_list);
-int _print_octal(va_list);
-int _print_hex_lower(va_list);
-int _print_hex_upper(va_list);
-int _print_pointer(va_list);
+int printf_hex_aux(unsigned long int num);
+int printf_pointer(va_list val);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _putchar(char c);
 
-#endif /* MAIN_H */
+#endif
